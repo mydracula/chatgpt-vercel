0f8e959b238678f615097dbc42da9f6627e8d130
2458578637 <2458578637@qq.com>
2023-07-12 00:11:00 +0800

    modified:   src/pages/api/index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -90,7 +90,9 @@ export const post: APIRoute = async context => {
       openaiAPIUrl
     } = body
 
-    const baseURL = openaiAPIUrl.replace(/^https?:\/\//, "") === 'api.openai.com' ? `api.openai.com/v1/chat/completions` : openaiAPIUrl;
+    const baseURL = openaiAPIUrl.replace(
+      /^https?:\/\//,
+      "")
 
     if (pwd && pwd !== password) {
       throw new Error("密码错误，请联系网站管理员。")
@@ -140,11 +142,11 @@ export const post: APIRoute = async context => {
     const decoder = new TextDecoder()
 
     const rawRes = await fetchWithTimeout(
-      `https://${baseURL}`,
+      `https://${baseURL}/v1/chat/completions`,
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${`apiKey`}`
+          Authorization: `Bearer ${apiKey}`
         },
         timeout: !timeout || Number.isNaN(timeout) ? 30000 : timeout,
         method: "POST",
