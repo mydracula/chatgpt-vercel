29519ba8d2572f4d2c0f1a336fd2b95520b819c3
2458578637 <2458578637@qq.com>
2023-07-11 22:44:37 +0800

    modified:   .env.example 	modified:   src/components/SettingAction.tsx 	modified:   src/system.ts 	modified:   src/types.ts

--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -65,7 +65,7 @@ export default function SettingAction(props: {
                 })
               }}
             >
-              <option value="gpt-3.5-turbo">gpt-3.5-turbo(4k)</option>
+              <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo(16k)</option>
               <option value="gpt-4">gpt-4(8k)</option>
               <option value="gpt-4-32k">gpt-4(32k)</option>
             </select>
